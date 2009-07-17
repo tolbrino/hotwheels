8dca95a1f8e80e9d09c39158577588b40663a313
joelr <joelr1@gmail.com>
2009-07-17 21:42:53 +0100

    courtesy of cliff moon

--- src/common.erl
+++ src/common.erl
@@ -25,7 +25,25 @@
 
 random_token() ->
     Term = term_to_binary({node(), make_ref()}),
-    Token = binary_to_list(erlang:md5(Term)),
-    lists:flatten([io_lib:format("~2.16.0b", [N]) || N <- Token]).
+    Digest = erlang:md5(Term),
+    binary_to_hex(Digest).
+ 
+binary_to_hex(Bin) when is_binary(Bin) ->
+    [oct_to_hex(N) || <<N:4>> <= Bin].
 
-
+oct_to_hex(0) -> $0;
+oct_to_hex(1) -> $1;
+oct_to_hex(2) -> $2;
+oct_to_hex(3) -> $3;
+oct_to_hex(4) -> $4;
+oct_to_hex(5) -> $5;
+oct_to_hex(6) -> $6;
+oct_to_hex(7) -> $7;
+oct_to_hex(8) -> $8;
+oct_to_hex(9) -> $9;
+oct_to_hex(10) -> $a;
+oct_to_hex(11) -> $b;
+oct_to_hex(12) -> $c;
+oct_to_hex(13) -> $d;
+oct_to_hex(14) -> $e;
+oct_to_hex(15) -> $f.
